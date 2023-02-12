3156fd82f66b759e82e929f45a501c1e23270e95
RossolD <grishadanil0@gmail.com>
2023-02-12 14:16:53 +0200

    UserService for loading, creating deleting etc

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -13,12 +13,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
     @Autowired
     private RoleRepository roleRepository;
@@ -33,20 +34,56 @@ public class UserService implements UserDetailsService {
         return UserDetailsImpl.build(user);
     }
 
-    public User userCreate(User user){
-        Set<Role> roles = new HashSet<>();
-        Role userRole = roleRepository.findByName("USER")
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-        roles.add(userRole);
-        user.setRoles(roles);
+    public boolean saveUser(User user, boolean update) {
+        User userFromDB = userRepository.findByLogin(user.getLogin());
+
+        if (userFromDB != null) {
+            return false;
+        }
+
+        if (!update) {
+            String roleUser = "USER";
+            Set<Role> roles = new HashSet<>();
+            Role userRole = roleRepository.findByName(roleUser)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+            user.setRoles(roles);
+        }
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return user;
+        userRepository.save(user);
+        return true;
     }
 
-    public void userDelete(User user) {
-        Set<Role> roles = new HashSet<>();
-        Role userRole = roleRepository.findByName("USER")
+    public void deleteUser(long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        Role userRole = roleRepository.findByName(user.getRoles().iterator().next().getName())
                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+
         user.removeRole(userRole);
+        userRepository.delete(user);
+    }
+
+    public List<User> findAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    public List<User> sortAllUsersByAsc() {
+        return (List<User>) userRepository.ascSorted();
+    }
+
+    public List<User> sortAllUsersByDesc() {
+        return (List<User>) userRepository.descSorted();
+    }
+
+    public User findUserByLogin(String value) {
+        return userRepository.findByLogin(value);
+    }
+
+    public User findUserById(long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
     }
 }
