6841e7179de5c4989671c95a183c6a7e6e15ac2f
RossolD <grishadanil0@gmail.com>
2023-02-12 14:19:39 +0200

    Role Entity

---
+++ src/main/java/com/example/demo/domain/Role.java
@@ -0,0 +1,34 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    public Role() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+}
