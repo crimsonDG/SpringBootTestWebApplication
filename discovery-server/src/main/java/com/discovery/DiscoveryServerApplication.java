9702234d67b36fcd0e6db595308392de8c84632a
RossolD <grishadanil0@gmail.com>
2023-04-14 19:56:37 +0300

    Adapted service to configure server and spring-boot-admin

--- discovery-server/src/main/java/com/discovery/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/discovery/DiscoveryServerApplication.java
@@ -3,9 +3,11 @@ package com.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
+@ComponentScan({"com.management.security"})
 public class DiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
