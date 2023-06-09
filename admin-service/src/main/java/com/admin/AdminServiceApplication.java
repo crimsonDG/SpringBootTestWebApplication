2ae712d04d21d2611bd8b7370f1a5ef73d98f462
RossolD <grishadanil0@gmail.com>
2023-06-09 17:17:31 +0300

    Remove @EnableEurekaClient

--- admin-service/src/main/java/com/admin/AdminServiceApplication.java
+++ admin-service/src/main/java/com/admin/AdminServiceApplication.java
@@ -5,14 +5,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients(basePackages = "com.core.client")
 @EntityScan({"com.core.domain"})
 @EnableJpaRepositories({"com.core.repository"})
--- auth-service/src/main/java/com/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/auth/AuthServiceApplication.java
@@ -5,13 +5,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EntityScan({"com.core.domain"})
 @EnableJpaRepositories({"com.core.repository"})
 @ComponentScans({
