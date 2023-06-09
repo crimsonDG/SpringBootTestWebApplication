6472a63232fac22500f4920c85003fcd12bc0c31
RossolD <grishadanil0@gmail.com>
2023-06-09 18:00:58 +0300

    Config Redis with Redisson

--- security/src/main/java/com/security/redis/RedisConfig.java
+++ security/src/main/java/com/security/redis/RedisConfig.java
@@ -1,6 +1,10 @@
 package com.security.redis;
 
-import org.springframework.beans.factory.annotation.Value;
+import jakarta.annotation.PostConstruct;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -8,49 +12,55 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.Jedis;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
-    @Value("${REDIS_HOST}")
-    private String redisHost;
-
-    @Value("${REDIS_PORT}")
-    private int redisPort;
+    private final String redisAddress = "redis://" + "redis" + ":" + 6379;
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
-        return new JedisConnectionFactory(configuration);
+    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
+        return new RedissonConnectionFactory(redisson);
     }
 
-    //Redis Cache Manager Configuration
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(redisAddress)
+                .setConnectionPoolSize(10)
+                .setConnectionMinimumIdleSize(5)
+                .setIdleConnectionTimeout(5000);
+        return Redisson.create(config);
+    }
+
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory factory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
         RedisCacheConfiguration redisCacheConfiguration = config
                 .serializeKeysWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
-        return RedisCacheManager.builder(factory).cacheDefaults(redisCacheConfiguration)
-                .build();
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(factory).cacheDefaults(redisCacheConfiguration).build();
     }
 
     @PostConstruct
     public void clearCache() {
-        Jedis jedis = new Jedis(redisHost, redisPort, 1000);
-        jedis.flushAll();
-        jedis.close();
+        Config config = new Config();
+        config.useSingleServer().setAddress(redisAddress);
+        RedissonClient redisson = Redisson.create(config);
+        redisson.getKeys().flushdb();
+        redisson.shutdown();
     }
 
 }
