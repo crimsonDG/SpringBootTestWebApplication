3b5696b36aebf4cae99274a3d9d766af421305d2
RossolD <grishadanil0@gmail.com>
2023-06-09 18:15:02 +0300

    Exception details

---
+++ core/src/main/java/com/core/exception/response/ExceptionInfo.java
@@ -0,0 +1,27 @@
+package com.core.exception.response;
+
+import java.util.Date;
+
+public class ExceptionInfo {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ExceptionInfo(Date timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
