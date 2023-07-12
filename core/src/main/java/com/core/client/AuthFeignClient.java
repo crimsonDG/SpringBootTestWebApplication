a2cd4fdddf31a36f1c9609618601bf6249d5f3cd
RossolD <grishadanil0@gmail.com>
2023-07-12 14:30:00 +0300

    Adapt to new user entity logic

--- core/src/main/java/com/core/client/AuthFeignClient.java
+++ core/src/main/java/com/core/client/AuthFeignClient.java
@@ -1,16 +1,15 @@
 package com.core.client;
 
 import com.core.client.config.AuthFeignConfig;
-import com.core.model.template.UserAccessDto;
-import com.core.model.template.UserTokenDto;
+import com.core.model.template.UserAccess;
+import com.core.model.template.UserToken;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//Todo: make new client with new functional
 @FeignClient(name = "auth-service", configuration = AuthFeignConfig.class)
 public interface AuthFeignClient {
 
-    @PostMapping("/auth/token")
-    UserTokenDto auth(@RequestBody UserAccessDto userAccessDto);
+    @PostMapping("auth/keycloak/token")
+    UserToken auth(@RequestBody UserAccess userAccess);
 }
