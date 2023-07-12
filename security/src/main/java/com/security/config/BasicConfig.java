ec9287a13a3e82c3ba8b48eba619a7d30060a0d7
RossolD <grishadanil0@gmail.com>
2023-07-12 14:33:07 +0300

    Changed configuration class according to new user entity

--- security/src/main/java/com/security/config/BasicConfig.java
+++ security/src/main/java/com/security/config/BasicConfig.java
@@ -1,15 +1,14 @@
 package com.security.config;
 
 import com.core.client.AuthFeignClient;
-import com.core.repository.RoleRepository;
-import com.core.repository.UserRepository;
-import com.security.service.UserService;
+import com.core.repository.*;
+import com.security.common.KeycloakValues;
+import com.security.service.KeycloakUserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BasicConfig {
@@ -17,25 +16,39 @@ public class BasicConfig {
     @Autowired(required = false)
     private AuthFeignClient authFeignClient;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
+    @Autowired
+    private KeycloakEntityRepository keycloakEntityRepository;
+
+    @Autowired
+    private KeycloakRoleRepository keycloakRoleRepository;
+
+    @Autowired
+    private KeycloakCredentialRepository keycloakCredentialRepository;
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public KeycloakUserService keycloakUserService() {
+        return new KeycloakUserService(authFeignClient, modelMapper(), keycloakEntityRepository, keycloakRoleRepository, keycloakCredentialRepository, keycloakValues());
     }
 
     @Bean
-    public UserService userService() {
-        return new UserService(authFeignClient, userRepository, roleRepository, passwordEncoder(), modelMapper());
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
+    @Bean
+    public KeycloakValues keycloakValues() {
+        return new KeycloakValues(
+                "ddd02889-9b44-4fc6-a5f6-161af2d8d02d",
+                "demo",
+                "{\"hashIterations\":27500,\"algorithm\":\"pbkdf2-sha256\",\"additionalParameters\":{}}",
+                "password",
+                "custom",
+                "http://keycloak:8080/realms/demo/protocol/openid-connect/token",
+                "demo-client");
+    }
 }
