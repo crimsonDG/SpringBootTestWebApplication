9846c5539d7c0c74679c35508ad666265b105ab0
RossolD <grishadanil0@gmail.com>
2023-07-12 14:31:10 +0300

    Changed migration according to new user entity

--- database-migration/src/main/resources/db.migration/V1__Init_DB.sql
+++ database-migration/src/main/resources/db.migration/V1__Init_DB.sql
@@ -1,27 +1,46 @@
-create table users (
-    user_id bigserial not null,
-    email varchar(255) not null,
-    login varchar(255) not null,
-    password varchar(255) not null,
-    primary key (user_id)
+create table user_entity (
+    id varchar(255) not null,
+    email varchar(255),
+    email_constraint varchar(255),
+    email_verified boolean not null,
+    enabled boolean not null,
+    federation_link varchar(255),
+    first_name varchar(255),
+    last_name varchar(255),
+    realm_id varchar(255) not null,
+    username varchar(255) not null,
+    created_timestamp bigint not null,
+    service_account_client_link varchar(255),
+    not_before integer not null,
+    primary key (id)
 );
 
-create table roles (
-    role_id serial not null,
-    name varchar(255) not null,
-    primary key (role_id)
+create table credential (
+    id varchar(255) not null,
+    salt bytea,
+    type varchar(255) not null,
+    user_id varchar(255) not null,
+    created_date bigint not null,
+    user_label varchar(255),
+    secret_data text not null,
+    credential_data text not null,
+    priority integer not null,
+    primary key (id),
+    constraint fk_user
+      foreign key(user_id)
+	    references user_entity(id)
 );
 
-create table users_roles (
-    user_id bigint not null,
-    role_id integer not null,
-    primary key (user_id, role_id)
+create table user_role_mapping (
+    role_id varchar(255) not null,
+    user_id varchar(255) not null,
+    primary key (role_id, user_id)
 );
 
-alter table if exists users_roles
+alter table if exists user_role_mapping
     add constraint role_fk
-    foreign key (role_id) references roles;
+    foreign key (id) references keycloak_role;
 
-alter table if exists users_roles
+alter table if exists user_role_mapping
     add constraint user_fk
-    foreign key (user_id) references users;
+    foreign key (id) references user_entity;
--- database-migration/src/main/resources/db.migration/V2__Add_Users.sql
+++ database-migration/src/main/resources/db.migration/V2__Add_Users.sql
@@ -1,25 +1,73 @@
-insert into users(user_id, login, email, password)
-    values (1, 'SpitFire', 'spitfire@gmail.com', '$2a$04$.NsXsT4omiHMlvgJqUqAg.pjq61qRK14da.9IXi0yRULwmdtl71Wq');
+insert into user_entity(id, email, email_constraint, email_verified, enabled, realm_id, username, created_timestamp, not_before)
+    values ('eae85ea6-1be7-46df-8231-ac355074fdf4',
+            'stoneforever@mmail.uk',
+            'stoneforever@mmail.uk',
+            'f',
+            't',
+            'ddd02889-9b44-4fc6-a5f6-161af2d8d02d',
+            'kc_admin',
+            1689151707934,
+            0);
 
-insert into users(user_id, login, email, password)
-    values (2, 'Claw', 'motorclaw@somemail.com', '$2a$04$PCKSxFh.gGTzus2NHjMye.78D96rAysIV.ntp5MoSvGztMI91qJNK');
+insert into user_entity(id, email, email_constraint, email_verified, enabled, realm_id, username, created_timestamp, not_before)
+    values ('f156142e-4cf1-4a6d-8534-2cb71dc0f709',
+            'spitfire@gmail.com',
+            'spitfire@gmail.com',
+            'f',
+            't',
+            'ddd02889-9b44-4fc6-a5f6-161af2d8d02d',
+            'spitfire',
+            1689151753526,
+            0);
 
-insert into users(user_id, login, email, password)
-    values (3, 'Admin', 'stoneforever@mmail.uk', '$2a$04$Qk8KFO0qgcjUQBsRXcYWj.wyvOQ0kBuWrvV0miZM1QHqcBOpX0X4e');
+insert into user_entity(id, email, email_constraint, email_verified, enabled, realm_id, username, created_timestamp, not_before)
+    values ('6fbba4dd-b094-4e46-bdcc-e018dff356c9',
+            'motorclaw@somemail.com',
+            'motorclaw@somemail.com',
+            'f',
+            't',
+            'ddd02889-9b44-4fc6-a5f6-161af2d8d02d',
+            'claw',
+            1689151837444,
+            0);
 
-SELECT setval(pg_get_serial_sequence('users', 'user_id'), coalesce(max(user_id)+1, 1), false) FROM users;
 
-insert into roles(role_id, name)
-    values (1, 'USER');
+insert into credential(id, type, user_id, created_date, user_label, secret_data, credential_data, priority)
+    values ('33c89603-cff3-42c1-bfbc-58e869192e0c',
+            'password',
+            'f156142e-4cf1-4a6d-8534-2cb71dc0f709',
+            1689151804809,
+            'My password',
+            '{"value":"lqNFGi5x1bZ3Y6SS2ImVNqiiuJL/U1/Cj2Fl6Wfti98=","salt":"8CYQnjxwSB+vh+ncaithGA==","additionalParameters":{}}',
+            '{"hashIterations":27500,"algorithm":"pbkdf2-sha256","additionalParameters":{}}',
+            10);
 
-insert into roles(role_id, name)
-    values (2, 'ADMIN');
+insert into credential(id, type, user_id, created_date, user_label, secret_data, credential_data, priority)
+    values ('3101f1f7-a635-4a58-8f56-9514e939093c',
+            'password',
+            'eae85ea6-1be7-46df-8231-ac355074fdf4',
+            1689151818041,
+            'My password',
+            '{"value":"BkaubEGIIc6xfWKI1Vo5bNK16FkwgWG5hPu+vhiKcAA=","salt":"t4weHMt9ydil9cYDER3n0Q==","additionalParameters":{}}',
+            '{"hashIterations":27500,"algorithm":"pbkdf2-sha256","additionalParameters":{}}',
+            10);
 
-insert into users_roles(user_id, role_id)
-    values (1, 1);
+insert into credential(id, type, user_id, created_date, user_label, secret_data, credential_data, priority)
+    values ('b30c2956-aebf-469c-afd7-1e96de999054',
+            'password',
+            '6fbba4dd-b094-4e46-bdcc-e018dff356c9',
+            1689151850051,
+            'My password',
+            '{"value":"Z8rZiYDQmqqL+VWtKGIQjlOqaPiOh+bnOEmIS8zGfIU=","salt":"Me3ws8yUDD+uouMptbrp2w==","additionalParameters":{}}',
+            '{"hashIterations":27500,"algorithm":"pbkdf2-sha256","additionalParameters":{}}',
+            10);
 
-insert into users_roles(user_id, role_id)
-    values (2, 1);
 
-insert into users_roles(user_id, role_id)
-    values (3, 2);
+insert into user_role_mapping(role_id, user_id)
+    values ('f9db96d8-6e9e-4f89-9329-89134064c816', 'eae85ea6-1be7-46df-8231-ac355074fdf4');
+
+insert into user_role_mapping(role_id, user_id)
+    values ('9256706e-cee5-4975-ac5b-d52f1f70ca5f', 'f156142e-4cf1-4a6d-8534-2cb71dc0f709');
+
+insert into user_role_mapping(role_id, user_id)
+    values ('9256706e-cee5-4975-ac5b-d52f1f70ca5f', '6fbba4dd-b094-4e46-bdcc-e018dff356c9');
