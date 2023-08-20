afe0960d92f00b4b453307501b6619b49a32c620
RossolD <grishadanil0@gmail.com>
2023-08-20 18:32:53 +0300

    Define common and specific configurations file

--- security/src/main/java/com/security/config/basic/BasicConfig.java
+++ security/src/main/java/com/security/config/basic/BasicConfig.java
@@ -1,16 +1,22 @@
-package com.security.config;
+package com.security.config.basic;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebClientConfig {
+public class BasicConfig {
 
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
