6a694f78eaab26e7cdfacddd25e646177ab83cd8
RossolD <grishadanil0@gmail.com>
2023-08-20 19:21:12 +0300

    Music info of users DTO

---
+++ security/src/main/java/com/security/service/response/MusicProfile.java
@@ -0,0 +1,27 @@
+package com.security.service.response;
+
+import com.redis.model.MusicDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MusicProfile implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 123L;
+
+    private String id;
+    private String username;
+    private String email;
+    private List<MusicDto> songs;
+
+}
