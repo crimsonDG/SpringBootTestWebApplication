acb253f04e2b31034c3066620a9bce5cc064480a
crimsonDG <grishadanil0@gmail.com>
2023-10-28 23:02:04 +0300

    Adapt to Song class

--- music-service/src/main/java/com/music/service/RedisMusicService.java
+++ music-service/src/main/java/com/music/service/RedisMusicService.java
@@ -1,6 +1,6 @@
 package com.music.service;
 
-import com.redis.model.MusicDto;
+import com.redis.model.SongDto;
 import com.redis.service.RedissonService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,26 +16,26 @@ public class RedisMusicService {
 
     private final RedissonService redissonService;
 
-    public void saveSong(MusicDto musicDto) {
-        redissonService.putData(RedissonService.STORAGE, Collections.singletonList(musicDto));
+    public void saveSong(SongDto songDto) {
+        redissonService.putData(RedissonService.STORAGE, Collections.singletonList(songDto));
     }
 
-    public String deleteSong(MusicDto musicDto) {
-        List<MusicDto> songs = redissonService.getData(RedissonService.STORAGE);
+    public String deleteSong(SongDto songDto) {
+        List<SongDto> songs = redissonService.getData(RedissonService.STORAGE);
 
-        Optional<MusicDto> songToRemove = songs.stream()
-                .filter(song -> musicDto.getId().equals(song.getId()))
+        Optional<SongDto> songToRemove = songs.stream()
+                .filter(song -> songDto.getId().equals(song.getId()))
                 .findFirst();
 
         if (songToRemove.isPresent()) {
-            List<MusicDto> updatedSongs = new ArrayList<>(songs);
+            List<SongDto> updatedSongs = new ArrayList<>(songs);
 
             if (updatedSongs.remove(songToRemove.get())) {
                 songs.addAll(updatedSongs);
-                redissonService.putData(musicDto.getId(), updatedSongs);
-                return String.format("\"%s\" removed from the main library!", musicDto.getName());
+                redissonService.putData(songDto.getId(), updatedSongs);
+                return String.format("\"%s\" removed from the main library!", songDto.getName());
             }
         }
-        return String.format("\"%s\" does not exist in the main library!", musicDto.getName());
+        return String.format("\"%s\" does not exist in the main library!", songDto.getName());
     }
 }
