2a664d83a8148e5ee8b5bea67ac4829397b7bd5b
crimsonDG <grishadanil0@gmail.com>
2023-10-28 23:03:35 +0300

    Adapt to Song class

---
+++ redis-config/src/main/java/com/redis/model/SongDto.java
@@ -0,0 +1,24 @@
+package com.redis.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SongDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 123L;
+
+    private String id;
+    private String name;
+    private String artist;
+    private String album;
+    private int year;
+    private String duration;
+}
