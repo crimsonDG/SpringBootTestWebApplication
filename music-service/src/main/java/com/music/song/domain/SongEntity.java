a1ffbdc02fcf2ecdd1f4a68ceac7b213a0ab7c82
crimsonDG <grishadanil0@gmail.com>
2023-10-28 23:02:22 +0300

    Adapt to Song class

---
+++ music-service/src/main/java/com/music/song/domain/SongEntity.java
@@ -0,0 +1,30 @@
+package com.music.song.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "songs")
+public class SongEntity {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String artist;
+
+    private String album;
+
+    private int year;
+
+    private String duration;
+
+}
