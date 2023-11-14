31d89d8c13ce6fc7570abbefd5d25aab4e6de8c3
crimsonDG <grishadanil0@gmail.com>
2023-11-14 16:14:45 +0200

    Create temp file

--- core/src/main/java/com/core/pdf/PdfGenerator.java
+++ core/src/main/java/com/core/pdf/PdfGenerator.java
@@ -1,10 +1,10 @@
 package com.core.pdf;
 
-import com.core.util.OsDefiner;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -12,17 +12,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.UUID;
 
 @Service
 public class PdfGenerator {
 
     @SneakyThrows
     public static byte[] generatePdfFromHtml(@NonNull String htmlFile) {
-        String defaultPath = OsDefiner.getDefaultPath(OsDefiner.getOperatingSystemType());
-        String filePath = defaultPath + "download.html";
-        Path pathHtml = Paths.get(filePath);
-        Path pathPdf = Paths.get(filePath.replace("html", "pdf"));
-
+        File htmlTempFile = File.createTempFile(UUID.randomUUID().toString(), ".html");
+        Path pathHtml = Path.of(htmlTempFile.getPath());
+        Path pathPdf = Paths.get(htmlTempFile.getPath().replace("html", "pdf"));
         try {
             String htmlContent = new Scanner(Objects.requireNonNull(PdfGenerator.class.getClassLoader().getResourceAsStream(htmlFile)),
                     StandardCharsets.UTF_8)
